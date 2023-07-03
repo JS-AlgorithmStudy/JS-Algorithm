7257fdd91ffafc6f81f24c10b975753bdda9ceba
yueunkim <yueunkim0605@gmail.com>
2023-07-03 22:57:41 +0900

    백준_숫자카드2

---
+++ yueunkim/20230704/p_131701_연속부분수열합의개수.js
@@ -0,0 +1,20 @@
+function solution(elements) {
+  const result = new Set();
+
+  //i는 묶을 요소의 개수
+  for (let i = 1; i <= elements.length; i++) {
+    //j는 묶을 요소들의 시작 인덱스
+    for (let j = 0; j < elements.length; j++) {
+      let sum = 0;
+
+      //k는 더해질 요소들의 인덱스
+      for (let k = 0; k < i; k++) {
+        //인덱스가 배열의 길이를 넘어갈 경우 인덱스 순환
+        sum += elements[(j + k) % elements.length];
+      }
+      result.add(sum); //중복 아닌 것만 알아서 추가
+    }
+  }
+
+  return result.size;
+}
---
+++ yueunkim/20230704/p_92335_k진수에서소수개수구하기.js
@@ -0,0 +1,25 @@
+//소수 판별
+function isPrime(num) {
+  if (num <= 1) return false;
+
+  //숫자의 제곱근까지만 체크
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(n, k) {
+  let primeCount = 0; //소수 개수
+  let numbers = n.toString(k).split("0"); // n을 k진수로 변환 후 0을 기준으로 자름
+
+  numbers.forEach((number) => {
+    //비어있지 않은 요소만
+    if (number !== "") {
+      const num = +number; // 문자열을 숫자로 변환
+      if (isPrime(num)) primeCount++; //소수면 primeCount+1
+    }
+  });
+
+  return primeCount;
+}
