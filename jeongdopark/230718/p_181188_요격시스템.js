925ce84d718fae6759168a882f529260094acb27
jeongdopark <jdpark0816@gmail.com>
2023-07-18 10:01:07 +0900

    7/18

--- jeongdopark/230718/p_181188_요격시스템.js
+++ jeongdopark/230718/p_181188_요격시스템.js
@@ -6,13 +6,29 @@
 // 
 // solve)
 // 정렬이 필요함.
-// 
+// 오름차순 ? 내림차순 ?
 
 const solution = (targets) => {
-  const targets = targets.sort((a, b) => b[0] - a[0])
-  console.log(targets);
-  var answer = 0;
+  // 내림차순 , 오름차순으로 할 경우 딱 떨어지지 않음
+  // [ [ 11, 13 ], [ 10, 14 ], [ 5, 12 ], [ 4, 5 ], [ 4, 8 ], [ 3, 7 ], [ 1, 4 ] ]
+  const targets_sort = targets.sort((a, b) => b[0] - a[0])
+  // 가장 첫 번째 요격은 정렬된 첫 번째 시작점
+  let missile = targets_sort[0][0];
+  // 필요한 미사일 개수
+  var answer = 1;
+
+  // 정렬된 입력값 순회
+  for(let i=1; i<targets_sort.length; i++){
+    // [10, 14]
+    const [start, end] = targets_sort[i];
+    // 미사일이 현재 end값과 같거나 클 경우 미사일 업데이트 & 미사일 개수 +1
+    if(end <= missile){
+      missile = start;
+      answer += 1;
+    }
+  }
+  
   return answer;
 }
 
-solution([[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]])
+console.log(solution([[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]]));
